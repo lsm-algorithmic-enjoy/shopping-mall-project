4200a7c24f0b23ab5d05abc28a49446a8c05362e
seungmin-lee-dotcom <k07217@naver.com>
2022-11-20 23:48:48 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,44 @@
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+  <img src="${item.image}" alt="${item.type}" class="item__thumnail" />
+  <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
+loadItems().then((items) => {
+  displayItems(items);
+  setEventListeners(items);
+});
